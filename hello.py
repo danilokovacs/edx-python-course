0c8cc9b2377de20bf99df7caf9fb579f897c51c8
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-07-30 22:27:55 -0300

    learning print function passing arguments

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello, world")
+# Assign value from input to variable
+name = input("What`s your name? ")
+
+# Concatenation
+print("Hello, " + name)
+
+# Passing variable as argument making space automatically by sep property
+print("Hello,", name)
+
+"""
+    Multiline comment
+    Just testing how this works :)
+"""
