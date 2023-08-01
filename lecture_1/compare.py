44c84a50982d4278e910c1a37aca8c1e6ba88279
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-07-31 22:39:56 -0300

    using elif and else in conditional

--- lecture_1/compare.py
+++ lecture_1/compare.py
@@ -4,9 +4,9 @@ y = int(input("Enter second number: "))
 if x < y:
     print(f"{x} is less than {y}")
 
-if x > y:
+elif x > y:
     print(f"{x} is greater than {y}")
 
-if x == y:
+else:
     print(f"{x} is equal to {y}")
 
