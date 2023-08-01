93f4d57bd8ab70db01b274b5e02ae82ff75abf76
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-07-31 22:38:24 -0300

    first conditionals

---
+++ lecture_1/compare.py
@@ -0,0 +1,12 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+if x < y:
+    print(f"{x} is less than {y}")
+
+if x > y:
+    print(f"{x} is greater than {y}")
+
+if x == y:
+    print(f"{x} is equal to {y}")
+
