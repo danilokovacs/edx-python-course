92eacfbd633b72a518afa5ca1bdd58c7ef707d73
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-07-31 22:58:34 -0300

    making is_even() function to return and print by its result

--- lecture_1/parity.py
+++ lecture_1/parity.py
@@ -1,6 +1,12 @@
-x = int(input("What`s x? "))
+def main():
+    x = int(input("What`s x? "))
 
-if x % 2 == 0:
-    print(f"The number {x} is even")
-else:
-    print(f"The number {x} is odd")
+    if is_even(x):
+        print(f"The number {x} is even")
+    else:
+        print(f"The number {x} is odd")
+
+def is_even(x):
+    return x % 2 == 0
+
+main()
