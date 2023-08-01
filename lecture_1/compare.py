b19c079a828a316531ef22307a9a27b89cf6b391
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-07-31 22:42:36 -0300

    using logic gate on if statement

--- lecture_1/compare.py
+++ lecture_1/compare.py
@@ -1,12 +1,8 @@
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
 
-if x < y:
-    print(f"{x} is less than {y}")
-
-elif x > y:
-    print(f"{x} is greater than {y}")
-
+if x < y or x > y:
+    print(f"{x} is not equal to {y}")
 else:
     print(f"{x} is equal to {y}")
 
