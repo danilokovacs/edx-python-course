b6b02975d0c95fff11b203b5aa170d66be5bd781
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-08-01 23:41:24 -0300

    using list to iterate with for with IN

--- lecture_2/cat.py
+++ lecture_2/cat.py
@@ -1,9 +1,12 @@
-i = 3
-while i != 0:
-    print("meow")
-    i = i - 1
+# i = 3
+# while i != 0:
+#     print("meow")
+#     i = i - 1
 
-j = 0
-while j < 3:
+# j = 0
+# while j < 3:
+#     print("meow")
+#     j += 1
+
+for i in [0, 1, 2]:
     print("meow")
-    j += 1
