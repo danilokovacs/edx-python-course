75b4119776fc41e81da0871bbd5ba50c168885de
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-08-13 12:52:27 -0300

    using nested loops to draw a square by printing

--- lecture_2/mario.py
+++ lecture_2/mario.py
@@ -1,13 +1,18 @@
 def main():
-    print_column(3)
-    print_row(3)
+    print_square(3)
 
-def print_column(num):
-    for _ in range(num):
-        print("#")
+def print_square(size):
+    for i in range(size):
+        for _ in range(size):
+            print("#", end="")
+        print()
 
-def print_row(num):
-    for _ in range(num):
-        print("#", end="")
+# def print_column(num):
+#     for _ in range(num):
+#         print("#")
+
+# def print_row(num):
+#     for _ in range(num):
+#         print("#", end="")
 
 main()
