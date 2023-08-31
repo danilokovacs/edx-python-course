739f0b0f98e8d309505765e4b89dae552859e144
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-08-30 21:57:51 -0300

    its possible to pass when you catch an exception

--- lecture_3/number.py
+++ lecture_3/number.py
@@ -3,6 +3,7 @@ while True:
         x = int(input("What`s x? "))
         break
     except ValueError:
+        # we can use the work "pass" to just pass and not throw any exception
         print("Value inputed is not integer")
     # else:
     #     break
