c348a0e229e9c48385877ea2a9823d8f96d16f96
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-08-30 21:53:17 -0300

    inserting the verification on loop, break and print if x is an integer

--- lecture_3/hello.py
+++ lecture_3/hello.py
@@ -1,5 +1,10 @@
-try:
-    x = int(input("What`s x? "))
-    print(f"x is {x}")
-except ValueError:
-    print("Value inputed is not integer")
+while True:
+    try:
+        x = int(input("What`s x? "))
+        print(f"x is {x}")
+    except ValueError:
+        print("Value inputed is not integer")
+    else:
+        break
+
+print(f"x is {x}")
